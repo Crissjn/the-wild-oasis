741779fb870b3295653900dd967c207c72a4e3e9
Crissjn <cracus89@gmail.com>
2025-02-09 13:24:52 +0100

    added form for new cabin

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://wnkflyhijxygezvcyhuj.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indua2ZseWhpanh5Z2V6dmN5aHVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg5NjQxNDgsImV4cCI6MjA1NDU0MDE0OH0.Qx_R_zoMl9dFJPPtUJ90cw6lbbP5KCOflHCkOiUhsPY";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
