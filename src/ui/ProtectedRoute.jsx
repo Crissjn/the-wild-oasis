31883a15196876005e66cdedf148d6539b5111a7
Crissjn <cracus89@gmail.com>
2025-02-26 18:58:28 +0100

    user login implemented with protectedroute element

---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var() (--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+  //load authenticated user
+  const { user, isLoading, isAuthenticated } = useUser();
+
+  //if no user,redirect to  login
+  useEffect(
+    function () {
+      if (!isAuthenticated && !isLoading) navigate("/login");
+    },
+    [isAuthenticated, isLoading, navigate]
+  );
+
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+
+  //if a user is logged in render app
+
+  if (isAuthenticated) return children;
+}
+
+export default ProtectedRoute;
